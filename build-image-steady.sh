a760d4afc0e8dc72c2bb79ee3e3a7ba346ec399d
Alexander Veit <alexander.veit@gmx.net>
2024-10-01 21:19:25 +0200

    Intrexx 12.0.1
    Signed-off-by: Alexander Veit <alexander.veit@gmx.net>

--- build-image-steady.sh
+++ build-image-steady.sh
@@ -7,7 +7,8 @@ cd "${0%/*}"
 # build parameters
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/11.0.0.20230328.2d22937/intrexx-11.0.0.20230328.2d22937-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/11.9.2.20240417.72d177b/intrexx-11.9.2.20240417.72d177b-linux.tar.gz"
-DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.0.20240528.8adb14f/intrexx-12.0.0.20240528.8adb14f-linux.tar.gz"
+#DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.0.20240528.8adb14f/intrexx-12.0.0.20240528.8adb14f-linux.tar.gz"
+DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.1.20240918.badc4ef/intrexx-12.0.1.20240918.badc4ef-linux.tar.gz"
 #DOWNLOAD_URL="https://archive.dev.intrexx.com/dev/steady/latest/setup/intrexx-latest.tar.gz"
 
 SUITE=${1:-bookworm}
