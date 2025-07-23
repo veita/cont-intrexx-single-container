3262243b41a7994b6f66e0e2b9a8ed6372ab118c
Alexander Veit <alexander.veit@gmx.net>
2025-07-23 07:54:05 +0200

    Intrexx 12.1.1
    Signed-off-by: Alexander Veit <alexander.veit@gmx.net>

--- build-image-steady.sh
+++ build-image-steady.sh
@@ -7,12 +7,14 @@ cd "${0%/*}"
 # build parameters
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/11.0.0.20230328.2d22937/intrexx-11.0.0.20230328.2d22937-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/11.9.2.20240417.72d177b/intrexx-11.9.2.20240417.72d177b-linux.tar.gz"
+#DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/11.9.3.20250220.4bc3126/intrexx-11.9.3.20250220.4bc3126-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.0.20240528.8adb14f/intrexx-12.0.0.20240528.8adb14f-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.1.20240918.badc4ef/intrexx-12.0.1.20240918.badc4ef-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.2.20241204.4beaae5/intrexx-12.0.2.20241204.4beaae5-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.3.20250224.3292a0d/intrexx-12.0.3.20250224.3292a0d-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.4.20250409.549668d/intrexx-12.0.4.20250409.549668d-linux.tar.gz"
-DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.0.20250519.17e48e4/intrexx-12.1.0.20250519.17e48e4-linux.tar.gz"
+#DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.0.20250519.17e48e4/intrexx-12.1.0.20250519.17e48e4-linux.tar.gz"
+DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.1.20250716.237b96c/intrexx-12.1.1.20250716.237b96c-linux.tar.gz"
 
 SUITE=${1:-bookworm}
 TCP_PORTS="10180 10181 10182 10183 10184 10185"
