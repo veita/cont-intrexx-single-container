56c2e946e14aea2a949ea63d7a2f46d301ee198b
Alexander Veit <alexander.veit@gmx.net>
2025-09-10 23:37:06 +0200

    Intrexx 12.1.3
    Signed-off-by: Alexander Veit <alexander.veit@gmx.net>

--- build-image-steady.sh
+++ build-image-steady.sh
@@ -15,7 +15,8 @@ cd "${0%/*}"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.0.4.20250409.549668d/intrexx-12.0.4.20250409.549668d-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.0.20250519.17e48e4/intrexx-12.1.0.20250519.17e48e4-linux.tar.gz"
 #DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.1.20250716.237b96c/intrexx-12.1.1.20250716.237b96c-linux.tar.gz"
-DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.2.20250730.eb1a245/intrexx-12.1.2.20250730.eb1a245-linux.tar.gz"
+#DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.2.20250730.eb1a245/intrexx-12.1.2.20250730.eb1a245-linux.tar.gz"
+DOWNLOAD_URL="https://download.intrexx.com/intrexx/rolling/steady/12.1.3.20250909.943dd0b/intrexx-12.1.3.20250909.943dd0b-linux.tar.gz"
 
 SUITE=${1:-trixie}
 TCP_PORTS="10180 10181 10182 10183 10184 10185"
